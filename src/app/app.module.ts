dc0bc3ad6b04f5905a878040b6ebdb0ea60e4d58
jmendozama <juan.mendoza@karibu.cl>
2020-09-02 15:28:15 -0400

    [FEA]: implementacion de mapa y envio de correos con archivo csv de listado de scanners

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { IonicStorageModule } from '@ionic/storage';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx'
 
+import { File } from '@ionic-native/file/ngx';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,6 +31,8 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx'
     SplashScreen,
     BarcodeScanner,
     InAppBrowser,
+    File,
+    EmailComposer,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }    
   ],
   bootstrap: [AppComponent]
--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -3,6 +3,8 @@ import { Registro } from 'platforms/android/app/models/registro.model';
 import { Storage } from '@ionic/storage';
 import { NavController } from '@ionic/angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { File as ionFile } from '@ionic-native/file/ngx';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,9 @@ export class DataLocalService {
 
   constructor( private storage: Storage,
                private navCtrl: NavController,
-               private iab: InAppBrowser) {
+               private iab: InAppBrowser,
+               private file: ionFile,
+               private emailComposer: EmailComposer) {
 
     // Cargar registros
     // this.storage.get('registros')
@@ -56,7 +60,65 @@ export class DataLocalService {
       default:
         break;
     }
+  }
 
+  enviarCorreo(){
+
+    const arrTemp = [];
+    const titulos = 'Tipo, Formato, Creado en, Texto\n';
+    
+    arrTemp.push(titulos);
+    
+    this.guardados.forEach(registro => {
+      const linea = `${registro.type}, ${registro.format}, ${registro.created}, ${registro.text.replace(',',' ')}\n`
+      arrTemp.push(linea);
+    })
+
+    this.crearArchivoFisico(arrTemp.join(''));
+  }
+
+  crearArchivoFisico(text:string){
+
+    this.file.checkFile(this.file.dataDirectory, 'registros.csv')
+      .then(existe => {
+        console.log('Existe archivo ',existe);
+        return this.escribirEnArchivo(text);
+      }).catch(err => {
+        console.log('Archivo no existe');
+        this.file.createFile(this.file.dataDirectory, 'registros.csv', false) 
+          .then(creado => {
+            // false significa no reemplazar archivo
+            this.escribirEnArchivo(text);
+          }).catch(err => {
+            console.log('No se pudo crear el archivo');
+          })
+      })
+  }
+
+  async escribirEnArchivo(text:string) {
+    await this.file.writeExistingFile(this.file.dataDirectory, 'registros.csv', text);
+    
+    const archivo = `${this.file.dataDirectory}/registros.csv`
+    //console.log(this.file.dataDirectory + 'registros.csv');
+
+    const email = {
+      to: 'jfranciscomendozam@gmail.com',
+      //cc: '',
+      //bcc: [],
+      attachments: [
+        // 'file://img/logo.png',
+        // 'res://icon.png',
+        // 'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+        // 'file://README.pdf'
+        archivo
+      ],
+      subject: 'Backup scan',
+      body: 'Adjunto backup de sus scanner - <strong>QRScanner</strong>',
+      isHtml: true
+    }
+    
+    // Send a text message using default options
+    this.emailComposer.open(email); // tambien recibe una promesa 
   }
 
 }
